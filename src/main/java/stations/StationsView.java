bf878dd2563b35f2cc17bbbf4f1ec443ca0b6c97
Kendall-Taylor <s9004570@student.rmit.edu.au>
2016-04-12 12:00:20 +1000

    feat: Changed Station combo box to a JList

--- src/main/java/stations/StationsView.java
+++ src/main/java/stations/StationsView.java
@@ -9,6 +9,9 @@ import observations.ObservationsPresenter;
 import observations.ObservationsView;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,13 +24,13 @@ import java.util.List;
  *
  * UI for the stations list.
  */
-public class StationsView implements StationsContract.View, ActionListener {
+public class StationsView implements StationsContract.View, ActionListener, ListSelectionListener {
     // TODO Kendall to modify as necessary - delete comment when done.
 
     private StationsContract.UserActionsListener mActionsListener;
     private JProgressBar mJProgressBar;
     private JComboBox<State> mStatesComboList;
-    private JComboBox<Station> mStationsComboList;
+    private JList<Station> mStationsJList;
     private HashMap<String, Station> mStationHashMap = new HashMap<String, Station>();
     private ObservationsView mObservationsView;
     private ObservationsPresenter mObservationsPresenter;
@@ -81,19 +84,19 @@ public class StationsView implements StationsContract.View, ActionListener {
             mStationHashMap.put(stations.get(i).getCity(), stations.get(i));
         }
 
-        if (mStationsComboList != null) {
-            Main.MainWindow.getInstance().getStationsPanel().remove(mStationsComboList);
+        if (mStationsJList != null) {
+            Main.MainWindow.getInstance().getStationsPanel().remove(mStationsJList);
             Main.MainWindow.getInstance().getStationsPanel().repaint();
         }
-        mStationsComboList = new JComboBox<Station>();
-        DefaultComboBoxModel<Station> comboModel = new DefaultComboBoxModel<Station>();
+        mStationsJList = new JList<Station>();
+        DefaultListModel<Station> listModel = new DefaultListModel<Station>();
         for(Station s : stations) {
-            comboModel.addElement(s);
+            listModel.addElement(s);
         }
-        mStationsComboList.setModel(comboModel);
-        mStationsComboList.setSelectedIndex(0);
-        mStationsComboList.addActionListener(this);
-        Main.MainWindow.getInstance().getStationsPanel().add(mStationsComboList);
+        mStationsJList.setModel(listModel);
+        mStationsJList.setSelectedIndex(0);
+        mStationsJList.addListSelectionListener(this);
+        Main.MainWindow.getInstance().getStationsPanel().add(mStationsJList);
     }
 
     public void showStationAsFavourited(String stationId, boolean favourite) {
@@ -114,10 +117,20 @@ public class StationsView implements StationsContract.View, ActionListener {
             JComboBox<State> cb = (JComboBox<State>)e.getSource();
             String stateName = cb.getSelectedItem().toString();
             mActionsListener.loadStations(stateName, false, true); // second argument false means load non-favourite stations
-        } else if (e.getSource() == mStationsComboList) {
-            JComboBox<Station> cb = (JComboBox<Station>)e.getSource();
-            String stationName = cb.getSelectedItem().toString();
-            mActionsListener.openObservations(mStationHashMap.get(stationName));
+        } 
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+	if (e.getSource() == mStationsJList) {
+	    // Ensure the user has finished selecting
+	    if ( !e.getValueIsAdjusting()) {
+		JList<Station> list = (JList<Station>)e.getSource();
+		String stationName = list.getSelectedValue().toString();
+		mActionsListener.openObservations(mStationHashMap.get(stationName));
+	    }
         }
+	
     }
 }
