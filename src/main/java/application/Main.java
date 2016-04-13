b3322826c67a7d39b92e9dbb02f98a3fe0c526be
Kendall-Taylor <s9004570@student.rmit.edu.au>
2016-04-13 21:25:24 +1000

    feat: Added saving of windows settings between application settings.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -11,6 +11,10 @@ import java.awt.*;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.StringTokenizer;
+import java.util.prefs.Preferences;
 
 /**
  * Created by michael on 20/03/16.
@@ -71,6 +75,8 @@ public class Main {
         private static JScrollPane stationsScrollPane;
         private static JScrollPane favouritesScrollPane;
         
+        private static JFrame jFrame;
+        
         private static JMenuBar menubar;
         private JLabel stationName;
 
@@ -78,39 +84,20 @@ public class Main {
 
         private MainWindow() {
             // Container frame for the main window
-            JFrame jFrame;
-            jFrame = new JFrame("SEPT Weather App");
-            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             
-            // add window listener to persist data
-            /*jFrame.addComponentListener(new ComponentListener() {
+            jFrame = new JFrame("SEPT Weather App");          
+            applyPreferences();
 
-				@Override
-				public void componentResized(ComponentEvent e) {
-					System.out.println("Resized Height: " + e.getComponent().getHeight());
-					System.out.println("Resized Width: " + e.getComponent().getWidth());
-					
-				}
-
-				@Override
-				public void componentMoved(ComponentEvent e) {
-					System.out.println("Moved X: " + e.getComponent().getX());
-					System.out.println("Moved Y: " + e.getComponent().getY());
-				}
-
-				@Override
-				public void componentShown(ComponentEvent e) {
-					System.out.println("Shown: " + e.getSource());
-					
-				}
-
-				@Override
-				public void componentHidden(ComponentEvent e) {
-					System.out.println("Hidden: " + e.getSource());
-					
-				}
-            	
-            });*/
+            jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            jFrame.addWindowListener( new WindowAdapter() {
+                     // When the window is disposed, this event handler will
+                     // be notified.  It will save current settings to the
+                     // user's preferences and terminate the program.
+                public void windowClosed(WindowEvent evt) {
+                    savePreferences();
+                    System.exit(0);
+                }
+            });
             
             container = jFrame.getContentPane();
 
@@ -130,9 +117,10 @@ public class Main {
             createObservationsPanel();
 
             // Display the main window.
-            jFrame.setSize(frameWidth,frameHeight);
-            jFrame.setLocationRelativeTo(null);
+           
+
             jFrame.setVisible(true);
+            
         }
 
         
@@ -266,6 +254,89 @@ public class Main {
             menuItemFav2.setEnabled(false);
             menuFav.add(menuItemFav2);
             menubar.add(menuFav);
+        }
+        
+        /**
+         * Called when the program ends to try to save window bounds and current
+         * directory to user's preferences, so they can be restored the next
+         * time the program is run.  Since this is not a critical function, errors
+         * are ignored.  If an error occurs, the data simply won't be saved.
+         */
+        private static void savePreferences() {
+            try {
+                String pathName = "/sept2016";
+                   // The pathname uniquely identifies this program.  (It is unique
+                   // because it comes from the package name, which should not be used
+                   // for any other program under Java's package naming guidelines.
+                Preferences root = Preferences.userRoot();
+                Preferences node = root.node(pathName);
+                   // This "node" holds the preferences associated with the pathName.
+                   // Preferences take the form of key/value pairs, with put() and
+                   // get() operations for changing/reading values.
+                Rectangle bounds = jFrame.getBounds();
+                String boundsString = bounds.x + "," + bounds.y + "," 
+                                            + bounds.width + "," + bounds.height;
+                node.put("window.bounds", boundsString);  
+                
+            }
+            catch (Exception e) {
+            }
+        }
+        
+        
+        /**
+         * This method is called when the program starts to read and apply the
+         * preferences that were saved the last time the program ran.  Since
+         * this is not a critical function, errors are ignored.  If an error
+         * occurs, preferences will simply not be restored.
+         */
+        private static void applyPreferences() {
+            try {
+                String pathName = "/sept2016";  // Identifies prefs for this program.
+                Preferences root = Preferences.userRoot();
+                if (! root.nodeExists(pathName) )
+                    return;  // There are no saved prefs for this program yet.
+                Preferences node = root.node(pathName);
+                String boundsString = node.get("window.bounds",null);
+
+                if (boundsString != null) {
+                       // Try to restore window bounds, ignoring any error.
+                    String[] bounds = explode(boundsString,",");
+                    try {
+                        int x = Integer.parseInt(bounds[0]);
+                        int y = Integer.parseInt(bounds[1]);
+                        int w = Integer.parseInt(bounds[2]);
+                        int h = Integer.parseInt(bounds[3]);
+                        if (w > 5000 || h > 5000)
+                            throw new NumberFormatException();  // unreasonable values.
+                        jFrame.setBounds(x,y,w,h);
+                        
+                    }
+                    catch (NumberFormatException e) {
+                    }
+                }
+                else {
+                	jFrame.setSize(frameWidth, frameHeight);
+                	jFrame.setLocationRelativeTo(null);
+                }
+            }
+            catch (Exception e) {
+            }
+        }
+
+        /**
+         * A convenience method that breaks up a string into tokens, where
+         * the tokens are substrings separated by specified delimiters.
+         * For example, explode("ab,cde,f,ghij", ",") produces an array
+         * of the four substrings "ab"  "cde"  "f"  "ghi".
+         */
+        private static String[] explode(String str, String separators) {
+            StringTokenizer tokenizer = new StringTokenizer(str, separators);
+            int ct = tokenizer.countTokens();
+            String[] tokens = new String[ct];
+            for (int i = 0; i < ct; i++)
+                tokens[i] = tokenizer.nextToken();
+            return tokens;
         }
         
 		public JScrollPane getStationsScrollPane() {
